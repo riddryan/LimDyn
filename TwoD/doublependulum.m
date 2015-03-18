7afd1f6ad6aa3cac0efd0c14cdecf2ac706cd3d3
riddryan <riddryan@5402ddd8-c230-0410-baf7-84df76f2c218>
2015-03-18 02:42:26 +0000

    Fixed error for picking spring types
    git-svn-id: https://tools.assembla.com/svn/dynamicWalking/Branches/riddryan/ModelGeneration@643 5402ddd8-c230-0410-baf7-84df76f2c218

--- TwoD/doublependulum.m
+++ TwoD/doublependulum.m
@@ -8,8 +8,13 @@ r = r.addBody('bodyname','thigh');
 r = r.addJoint('shank','ground','hinge');
 r = r.addJoint('thigh','shank','hinge');
 
+r = r.addSpring('shank','thigh','type','angular');
+r = r.addDamper('shank','thigh','type','angular');
+
+
 r = r.getConstraintMatrix;
 r = r.getConstraintMatrixDot;
 r = r.getMassMatrix;
 r = r.getGravityForces;
+r = r.getExForces;
 
